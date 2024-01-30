0814f5c7cec3827a53dc4ed417a3733121232de0
Volodymyr Dolhov <vv.dolhov@gmail.com>
2024-01-30 05:45:10 -0800

    Update contact buttons styling and behavior

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let contact_button_header = document.querySelector('.contact-button-header');
+contact_button_header.addEventListener('mousemove', e => {
+  let rect = e.target.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+  contact_button_header.style.setProperty('--x', x + 'px');
+  contact_button_header.style.setProperty('--y', y + 'px');
+});
+
+
+let contact_button = document.querySelector('.contact-button');
+contact_button.addEventListener('mousemove', e => {
+  let rect = e.target.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+  contact_button.style.setProperty('--x', x + 'px');
+  contact_button.style.setProperty('--y', y + 'px');
+});
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
