3dc69c5c92375beff13b0dc1e6918d83413c0915
IgorOkhman <ioi@cc-interactive.com>
2024-02-04 23:16:06 +0200

    +

--- script.js
+++ script.js
@@ -17,27 +17,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// document
-//     .getElementById("contact-form")
-//     .addEventListener("submit", function (event) {
-//         event.preventDefault();
+document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
 
-//         const formData = new FormData(this);
+        const formData = new FormData(this);
 
-//         fetch(this.action, {
-//             method: "POST",
-//             body: formData, // sending the form data directly
-//             mode: "no-cors",
-//         })
-//             .then(() => {
-//                 // The response is opaque, so we don't try to parse it
-//                 alert("Message sent successfully!");
-//             })
-//             .catch((error) => {
-//                 console.error("Error:", error);
-//                 alert("There was an error sending the message.");
-//             });
-//     });
+        fetch(this.action, {
+            method: "POST",
+            body: formData, // sending the form data directly
+            mode: "no-cors",
+        })
+            .then(() => {
+                // The response is opaque, so we don't try to parse it
+                alert("Message sent successfully!");
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                alert("There was an error sending the message.");
+            });
+    });
 
 document.querySelectorAll('.block-name-container').forEach(container => {
     let interval;
